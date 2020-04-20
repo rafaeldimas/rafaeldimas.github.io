8674c0a57ed94d02ad0290fb06c50b41adedc349
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2020-04-19 23:31:35 -0300

    Updates

---
+++ precache-manifest.7fa79ab36d5c478340107a1be81ac09f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4581713740ed46a68a752bfdbb8ab2fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c1fc0ca13b4077562b6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea6cf2c10f4037151234",
+    "url": "/static/js/2.146dbb0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.146dbb0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1fc0ca13b4077562b6",
+    "url": "/static/js/main.0bc737c2.chunk.js"
+  },
+  {
+    "revision": "d26b5baf72c4d63bd5b9",
+    "url": "/static/js/runtime-main.13af4f7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
